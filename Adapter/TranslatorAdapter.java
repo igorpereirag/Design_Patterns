b4d8a8053da50ebfec8bd4e9d811e585e04ae6eb
IGOR <igorpereira1501@gmail.com>
2023-11-24 21:08:52 -0300

    UP

--- Adapter/TranslatorAdapter.java
+++ Adapter/TranslatorAdapter.java
@@ -3,18 +3,17 @@ package Adapter;
 // Adapter que adapta a interface antiga para a nova
 public class TranslatorAdapter implements ITranslationService {
     
-    private ITranslatorService itranslatorService;
+    private ITranslatorService translatorService;
 
-    public TranslatorAdapter(ITranslatorService itranslatorService) {
-        this.itranslatorService = itranslatorService;
+    public TranslatorAdapter(ITranslatorService translatorService) {
+        this.translatorService = translatorService;
     }
-
 
     @Override
     public String translateText(String text, String targetLanguage) {
        // Adaptação: Chama o método antigo com o idioma de origem fixo
         // pois a interface antiga não inclui um parâmetro para o idioma de origem
-        return itranslatorService.translate(text, "auto", targetLanguage);
+        return translatorService.translate(text, "auto", targetLanguage);
     }
     
 }
